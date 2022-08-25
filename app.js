280a5eb8046c665c5befcf5763b437c49e1acad7
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2022-08-24 20:02:09 -0400

    added function to display each book in the array as it is inserted

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const titleInput = document.querySelector('#book_title');
 const pageInput = document.querySelector('#pages');
 const checkboxInput = document.querySelector('#checkbox');
 const submitButton = document.querySelector('#submit');
+const bookContainer = document.querySelector('.book-container');
+const card = document.querySelector('.card');
 
 let myLibrary = [];
+// { title: 'randal', author: 'goat', pages: 21, read: true }
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -15,7 +18,7 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary() {
   //basic validation
-  if (titleInput.value || authorInput.value || pageInput.value) {
+  if (titleInput.value && authorInput.value && pageInput.value > 0) {
     const newBook = new Book(
       titleInput.value,
       authorInput.value,
@@ -24,11 +27,10 @@ function addBookToLibrary() {
     );
     //insert book into array
     myLibrary.push(newBook);
+    displayBook(myLibrary);
   } else {
     alert('please fill out all *required* fields');
   }
-
-  console.log(myLibrary);
   //reset form inputs
   document.getElementById('myform').reset();
 }
@@ -38,6 +40,33 @@ submitButton.addEventListener('click', addBookToLibrary);
 //Todos
 
 //loop through array and display each book on the page using cards
-//add a new book button that brings up a form allowing users to input details for new book
+let i = 0;
+function displayBook(arr) {
+  for (i; i < arr.length; i++) {
+    let div = document.createElement('div');
+    div.classList.add('card');
+    bookContainer.appendChild(div);
+
+    let ul = document.createElement('ul');
+    let li0 = document.createElement('li');
+    let li1 = document.createElement('li');
+    let li2 = document.createElement('li');
+    let li3 = document.createElement('li');
+
+    li0.innerText = `Title: ${arr[i].title}`;
+    li1.innerText = `Author: ${arr[i].author}`;
+    li2.innerText = `# Of Pages: ${arr[i].pages}`;
+    li3.innerText = `Read it already? ${arr[i].read}`;
+
+    ul.appendChild(li0);
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+    ul.appendChild(li3);
+    div.appendChild(ul);
+    // });
+  }
+}
+
+//add a new book button that brings up a form(modal) allowing users to input details for new book
 // add a button on each book's display to remove book from library
 //add a button on each book display to change its read status
