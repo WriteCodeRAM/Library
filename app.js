cb49be3d0359de5caadefd02a8db192a7fb6ade8
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2022-08-25 13:31:15 -0400

    added form to modal pop up

--- app.js
+++ app.js
@@ -2,9 +2,26 @@ const authorInput = document.querySelector('#author_name');
 const titleInput = document.querySelector('#book_title');
 const pageInput = document.querySelector('#pages');
 const checkboxInput = document.querySelector('#checkbox');
+
 const submitButton = document.querySelector('#submit');
 const bookContainer = document.querySelector('.book-container');
 const card = document.querySelector('.card');
+const form = document.querySelector('#myform');
+
+const newBookBtn = document.querySelector('.new-book');
+const modal = document.querySelector('.modal');
+
+newBookBtn.addEventListener('click', () => {
+  modal.classList.remove('hide');
+});
+
+modal.addEventListener('click', (e) => {
+  modal.classList.add('hide');
+});
+
+document.querySelector('.modal form').addEventListener('click', (e) => {
+  e.stopPropagation();
+});
 
 let myLibrary = [];
 // { title: 'randal', author: 'goat', pages: 21, read: true }
@@ -18,7 +35,12 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary() {
   //basic validation
-  if (titleInput.value && authorInput.value && pageInput.value > 0) {
+  if (
+    titleInput.value &&
+    authorInput.value &&
+    pageInput.value > 0 &&
+    pageInput <= 10000
+  ) {
     const newBook = new Book(
       titleInput.value,
       authorInput.value,
@@ -29,7 +51,7 @@ function addBookToLibrary() {
     myLibrary.push(newBook);
     displayBook(myLibrary);
   } else {
-    alert('please fill out all *required* fields');
+    alert('please fill out all *required* fields, max page number is 10000');
   }
   //reset form inputs
   document.getElementById('myform').reset();
@@ -53,6 +75,10 @@ function displayBook(arr) {
     let li2 = document.createElement('li');
     let li3 = document.createElement('li');
 
+    // programmatic approach
+    // for (const [key, value] of Object.entries(arr[i])) {
+    //   console.log(`${key}: ${value}`);
+    // }
     li0.innerText = `Title: ${arr[i].title}`;
     li1.innerText = `Author: ${arr[i].author}`;
     li2.innerText = `# Of Pages: ${arr[i].pages}`;
@@ -63,10 +89,19 @@ function displayBook(arr) {
     ul.appendChild(li2);
     ul.appendChild(li3);
     div.appendChild(ul);
+    modal.classList.add('hide');
     // });
   }
 }
 
 //add a new book button that brings up a form(modal) allowing users to input details for new book
+
+newBookBtn.addEventListener('click', showForm);
+
+function showForm() {
+  form.classList.add('show');
+}
+
 // add a button on each book's display to remove book from library
+
 //add a button on each book display to change its read status
